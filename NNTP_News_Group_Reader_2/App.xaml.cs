bf7fc6119ae264586ab32b1ca6d929cc3df1982a
Sunisima <lsun8525@gmail.com>
2025-10-28 12:08:19 +0100

    Add project files.

---
+++ NNTP_News_Group_Reader_2/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace NNTP_News_Group_Reader_2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ NNTP_Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace NNTP_Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void LoginWithCredentials_ReturnsOk()
+        {
+            Assert.True(true);
+        }
+    }
+}
