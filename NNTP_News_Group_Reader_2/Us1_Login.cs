77c03b11ed015459ebe370d191b6b7a5a1655f85
Sunisima <lsun8525@gmail.com>
2025-10-28 20:04:59 +0100

    Making correct test-first for US1.

---
+++ NNTP_News_Group_Reader_2/Us1_Login.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace NNTP_News_Group_Reader_2
+{
+    public class Us1_Login
+    {
+        // Red phase - it is supposed to fail and does.
+        //public string Login(string host, int portNo, string userName, string password)
+        //{
+        //    return string.Empty;
+        //}
+
+
+        // Green phase - Minimal implementation that returns expected value to make the unit test pass.
+        public string Login(string host, int portNo, string userName, string password)
+        {
+            return "281 Ok";
+        }
+    }
+}
