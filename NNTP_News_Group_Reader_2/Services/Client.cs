2db9adb788a772afa1ff21bf6695331f0a5543fd
Sunisima <lsun8525@gmail.com>
2025-10-28 19:53:54 +0100

    Making changes to how the project is setup! Making me way behind

--- NNTP_News_Group_Reader_2/Services/Client.cs
+++ NNTP_News_Group_Reader_2/Services/Client.cs
@@ -1,64 +1,25 @@
-﻿using System.Data;
-using System.IO;
+﻿using System.IO;
 using System.Net.Sockets;
-using System.Reflection.PortableExecutable;
 using System.Text;
 
 namespace NNTP_News_Group_Reader_2.Services
 {
     public class Client
     {
-        private TcpClient _client;
-        private NetworkStream _stream;
-        private StreamReader _streamReader;
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="userName"> The username of the user wanting to log in </param>
-        /// <param name="password"> the password for the user </param>
-        /// <returns></returns>
-        public string Login(string userName, string password)
-        {
-            // Login with user name and await server response
-            string userAuth = CommandsAndResponses($"AUTHINFO USER {userName}");
-
-            //Send password to server for validation
-            string passwordAuth = CommandsAndResponses($"AUTHINFO PASS {password}");
-
-            return passwordAuth;            
-        }
-    
-
-        /// <summary>
-        /// Connecting to the NNTP-server
-        /// </summary>
-        /// <param name="host"> The URL of the server </param>
-        /// <param name="portNo"> The port number to use to get connection </param>
-        public void ConnectToServer (string host, int portNo)
-        {
-            _client = new TcpClient(host, portNo);
-            _stream = _client.GetStream();
-            _streamReader = new StreamReader(_stream, Encoding.ASCII);
-        }
+        // Red phase - it is supposed to fail and does.
+        //public string Login (string host, int portNo, string userName, string password)
+        //{
+        //    return string.Empty;
+        //}
 
 
-        /// <summary>
-        /// Used for sending commands from client to server and recieving responses
-        /// </summary>
-        /// <param name="command"> The specific commands send to the server </param>
-        public string CommandsAndResponses (string command)
-        {
-            byte[] queries = Encoding.ASCII.GetBytes(command + "\r\n");
-            _stream.Write(queries, 0, queries.Length);
-            return _streamReader.ReadLine();
-        }
+        // Green phase - make the method return 281 Ok and parse the test with very simple code.
+        //public string Login (string host, int portNo, string userName, string password)
+        //{
+        //    return "281 Ok";
+        //}
 
-        public void Close()
-        {
-            _streamReader.Close();
-            _stream.Close();
-            _client.Close();
-        }
+
+        //Refactor phase - 
     }
 }
